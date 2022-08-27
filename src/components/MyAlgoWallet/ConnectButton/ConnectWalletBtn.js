31e1dd7530a04d523aa0c4c990101c8d64796145
Manuel Yang <myang@nativo.com>
2022-08-27 00:46:03 -0700

    fix runtime error

--- src/components/MyAlgoWallet/ConnectButton/ConnectWalletBtn.js
+++ src/components/MyAlgoWallet/ConnectButton/ConnectWalletBtn.js
@@ -1,22 +1,25 @@
-import React from 'react'
-import { loadStdlib } from '@reach-sh/stdlib'
-import MyAlgoConnect from '@reach-sh/stdlib/ALGO_MyAlgoConnect';
-import ConnectButton from '.';
-import { BodyText } from '../MyAlgoWallet.styles';
-const reach = loadStdlib("ALGO")
-reach.setWalletFallback(reach.walletFallback({
-    providerEnv: 'TestNet', MyAlgoConnect })); 
+import React from "react";
+import { loadStdlib } from "@reach-sh/stdlib";
+import { ALGO_MyAlgoConnect as MyAlgoConnect } from "@reach-sh/stdlib";
+import ConnectButton from ".";
+import { BodyText } from "../MyAlgoWallet.styles";
+const reach = loadStdlib("ALGO");
 
+reach.setWalletFallback(
+  reach.walletFallback({
+    providerEnv: "TestNet",
+    MyAlgoConnect,
+  })
+);
 
-const ConnectWalletButton = ({connectWallet,accountAddress, accountBal }) => {
+const ConnectWalletButton = ({ connectWallet, accountAddress, accountBal }) => {
+  return (
+    <div>
+      <ConnectButton connectWallet={connectWallet} />
+      <BodyText>Account Address: {accountAddress} </BodyText>
+      <BodyText>Account Balance: {accountBal}</BodyText>
+    </div>
+  );
+};
 
-    return(
-        <div>
-            <ConnectButton connectWallet = {connectWallet}/>
-                <BodyText>Account Address: {accountAddress} </BodyText>
-                <BodyText>Account Balance: {accountBal}</BodyText>
-        </div>
-    )
-}
-
-export default ConnectWalletButton
+export default ConnectWalletButton;
--- src/components/MyAlgoWallet/MyAlgoWallet.js
+++ src/components/MyAlgoWallet/MyAlgoWallet.js
@@ -1,67 +1,70 @@
-import React, {useState, useRef} from 'react'
-import { loadStdlib } from '@reach-sh/stdlib'
-import MyAlgoConnect from '@reach-sh/stdlib/ALGO_MyAlgoConnect';
-import ConnectWalletButton from './ConnectButton/ConnectWalletBtn';
-import TransferFund from './Transferfund';
-import FundAccount from './FundWallet';
-import myalgo from '../../assets/images/myaglo-logo.png'
-import { MyAlgoWalletMain } from './MyAlgoWallet.styles';
+import React, { useState, useRef } from "react";
+import { loadStdlib } from "@reach-sh/stdlib";
+import { ALGO_MyAlgoConnect as MyAlgoConnect } from "@reach-sh/stdlib";
+import ConnectWalletButton from "./ConnectButton/ConnectWalletBtn";
+import TransferFund from "./Transferfund";
+import FundAccount from "./FundWallet";
+import myalgo from "../../assets/images/myaglo-logo.png";
+import { MyAlgoWalletMain } from "./MyAlgoWallet.styles";
 
-const reach = loadStdlib("ALGO")
+const reach = loadStdlib("ALGO");
 
-reach.setWalletFallback(reach.walletFallback({
-  providerEnv: 'TestNet', MyAlgoConnect })); 
+reach.setWalletFallback(
+  reach.walletFallback({
+    providerEnv: "TestNet",
+    MyAlgoConnect,
+  })
+);
 
 const MyAlgoWallet = () => {
+  const account = useRef();
+  const balance = useRef();
 
-    const account = useRef()
-    const balance = useRef()
+  const [accountBal, setAccountBal] = useState(0);
+  const [accountAddress, setAccountAddress] = useState("");
 
-
-    const [accountBal, setAccountBal] = useState(0);
-    const [accountAddress, setAccountAddress] = useState('');
-
-
-    const connectWallet = async () =>{
-        try{
-            await getAccount()
-            await getBalance()
-                
-        }catch(err){
-            console.log(err)
-        }
+  const connectWallet = async () => {
+    try {
+      await getAccount();
+      await getBalance();
+    } catch (err) {
+      console.log(err);
     }
+  };
 
-    const getAccount = async () => {
-        try{
-           account.current = await reach.getDefaultAccount()
-            setAccountAddress(account.current.networkAccount.addr)
-            console.log("Account :" + account.current.networkAccount.addr)
-        }catch(err){
-            console.log(err)
-        }
+  const getAccount = async () => {
+    try {
+      account.current = await reach.getDefaultAccount();
+      setAccountAddress(account.current.networkAccount.addr);
+      console.log("Account :" + account.current.networkAccount.addr);
+    } catch (err) {
+      console.log(err);
     }
+  };
 
-    const getBalance = async () => {
-        try{
-              let rawBalance = await reach.balanceOf(account.current)
-                balance.current = reach.formatCurrency(rawBalance, 4)
-                setAccountBal(balance.current)
-            console.log("Balance :" + balance.current)
-        }catch(err){
-            console.log(err)
-        }
-      
+  const getBalance = async () => {
+    try {
+      let rawBalance = await reach.balanceOf(account.current);
+      balance.current = reach.formatCurrency(rawBalance, 4);
+      setAccountBal(balance.current);
+      console.log("Balance :" + balance.current);
+    } catch (err) {
+      console.log(err);
     }
+  };
 
-    return(
-        <MyAlgoWalletMain>
-            <img src= {myalgo} alt="My Algo" height= "70px"/>
-            <ConnectWalletButton accountAddress={accountAddress} connectWallet = {connectWallet} accountBal = {accountBal}/>
-            <TransferFund account = {account} getBalance = {getBalance} />
-            <FundAccount account = {account} getBalance = {getBalance}/>
-        </MyAlgoWalletMain>
-    )
-}
+  return (
+    <MyAlgoWalletMain>
+      <img src={myalgo} alt="My Algo" height="70px" />
+      <ConnectWalletButton
+        accountAddress={accountAddress}
+        connectWallet={connectWallet}
+        accountBal={accountBal}
+      />
+      <TransferFund account={account} getBalance={getBalance} />
+      <FundAccount account={account} getBalance={getBalance} />
+    </MyAlgoWalletMain>
+  );
+};
 
-export default MyAlgoWallet
+export default MyAlgoWallet;
